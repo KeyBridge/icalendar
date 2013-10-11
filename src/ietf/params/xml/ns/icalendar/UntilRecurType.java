1db069d2de3a34cfd32ca861ee4a85c7a78d5be2
Jesse Caulfield <jesse@caulfield.org>
2013-10-11 16:59:56 -0400

    add normalize to UntilRecurType dateTime element

--- src/ietf/params/xml/ns/icalendar/UntilRecurType.java
+++ src/ietf/params/xml/ns/icalendar/UntilRecurType.java
@@ -33,7 +33,83 @@ import javax.xml.datatype.XMLGregorianCalendar;
 })
 public class UntilRecurType {
 
-  private static final String UTC_PATTERN = "yyyyMMdd'T'HHmmss'Z'";
+  /**
+   * RFC5545 3.3.4. DATE
+   * <p/>
+   * Purpose: This value type is used to identify values that contain a calendar
+   * date.
+   * <p/>
+   * Format Definition: This value type is defined by the following notation:
+   * <pre>
+   * date               = date-value
+   * date-value         = date-fullyear date-month date-mday
+   * date-fullyear      = 4DIGIT
+   * date-month         = 2DIGIT        ;01-12
+   * date-mday          = 2DIGIT        ;01-28, 01-29, 01-30, 01-31
+   *                                    ;based on month/year
+   * </pre> Description: If the property permits, multiple "date" values are
+   * specified as a COMMA-separated list of values. The format for the value
+   * type is based on the [ISO.8601.2004] complete representation, basic format
+   * for a calendar date. The textual format specifies a four-digit year,
+   * two-digit month, and two-digit day of the month. There are no separator
+   * characters between the year, month, and day component text.
+   * <p/>
+   * No additional content value encoding (i.e., BACKSLASH character encoding,
+   * see Section 3.3.11) is defined for this value type.
+   * <p/>
+   * Example: The following represents July 14, 1997: 19970714
+   * <p/>
+   * @see <a href="http://tools.ietf.org/html/rfc5545#section-3.3.4">Date</a>
+   */
+  private static final String PATTERN_DATE = "yyyyMMdd";
+  /**
+   * RFC 5545 3.3.5. DATE-TIME
+   * <p/>
+   * Purpose: This value type is used to identify values that specify a precise
+   * calendar date and time of day.
+   * <p/>
+   * Format Definition: This value type is defined by the following notation:
+   * <pre>
+   * date-time  = date "T" time ;As specified in the DATE and TIME
+   *                            ;value definitions
+   * </pre> Description: If the property permits, multiple "DATE-TIME" values
+   * are specified as a COMMA-separated list of values. No additional content
+   * value encoding (i.e., BACKSLASH character encoding, see Section 3.3.11) is
+   * defined for this value type.
+   * <p/>
+   * The "DATE-TIME" value type is used to identify values that contain a
+   * precise calendar date and time of day. The format is based on the
+   * [ISO.8601.2004] complete representation, basic format for a calendar date
+   * and time of day. The text format is a concatenation of the "date", followed
+   * by the LATIN CAPITAL LETTER T character, the time designator, followed by
+   * the "time" format.
+   * <p/>
+   * The "DATE-TIME" value type expresses time values in three forms:
+   * <p/>
+   * The form of date and time with UTC offset MUST NOT be used. For example,
+   * the following is not valid for a DATE-TIME value:
+   * <p/>
+   * 19980119T230000-0800 ;Invalid time format
+   * <p/>
+   * FORM #1: DATE WITH LOCAL TIME <em>NOT SUPPORTED</em>
+   * <p/>
+   * FORM #2: DATE WITH UTC TIME
+   * <p/>
+   * The date with UTC time, or absolute time, is identified by a LATIN CAPITAL
+   * LETTER Z suffix character, the UTC designator, appended to the time value.
+   * For example, the following represents January 19, 1998, at 0700 UTC:
+   * <code>19980119T070000Z</code>
+   * <p/>
+   * The "TZID" property parameter MUST NOT be applied to DATE-TIME properties
+   * whose time values are specified in UTC.
+   * <p/>
+   * FORM #3: DATE WITH LOCAL TIME AND TIME ZONE REFERENCE <em>NOT
+   * SUPPORTED</em>
+   * <p/>
+   * @see <a
+   * href="http://tools.ietf.org/html/rfc5545#section-3.3.5">Date-Time</a>
+   */
+  private static final String PATTERN_DATE_TIME = "yyyyMMdd'T'HHmmss'Z'";
   @XmlTransient
   protected static final TimeZone TIME_ZONE = TimeZone.getTimeZone("UTC");
   @XmlSchemaType(name = "date")
@@ -45,7 +121,7 @@ public class UntilRecurType {
   }
 
   public UntilRecurType(GregorianCalendar dateTime) throws DatatypeConfigurationException {
-    this.dateTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(dateTime);
+    this.dateTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(dateTime).normalize();
   }
 
   public UntilRecurType(Calendar dateTime) throws DatatypeConfigurationException {
@@ -66,7 +142,7 @@ public class UntilRecurType {
    *                                        a Date
    */
   public UntilRecurType(String dateTime) throws DatatypeConfigurationException, ParseException {
-    setDateTime(new SimpleDateFormat(UTC_PATTERN).parse(dateTime));
+    setDateTime(new SimpleDateFormat(PATTERN_DATE).parse(dateTime));
   }
 
   /**
@@ -114,7 +190,7 @@ public class UntilRecurType {
   }
 
   public final void setDateTime(GregorianCalendar dateTime) throws DatatypeConfigurationException {
-    this.dateTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(dateTime);
+    this.dateTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(dateTime).normalize();
   }
 
   public final void setDateTime(java.util.Date dateTime) throws DatatypeConfigurationException {
@@ -186,7 +262,15 @@ public class UntilRecurType {
    */
   @Override
   public String toString() {
-    DateFormat sdf = new SimpleDateFormat(UTC_PATTERN);
-    return getCalendar() != null ? sdf.format(getCalendar().getTime()) : null;
+    DateFormat sdf;
+    if (dateTime != null) {
+      sdf = new SimpleDateFormat(PATTERN_DATE_TIME);
+      return getCalendar() != null ? sdf.format(getCalendar().getTime()) : null;
+    } else if (date != null) {
+      sdf = new SimpleDateFormat(PATTERN_DATE);
+      return getCalendar() != null ? sdf.format(getCalendar().getTime()) : null;
+    } else {
+      return null;
+    }
   }
 }
