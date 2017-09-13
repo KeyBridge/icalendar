36d969b0ec2cf707df544f3bd9b095136b8a5b5f
Druzinis <andrius.druzinis@gmail.com>
2017-09-13 13:28:18 +0200

    change infinite loop fix

--- src/main/java/ietf/params/xml/ns/icalendar/util/ICalendar.java
+++ src/main/java/ietf/params/xml/ns/icalendar/util/ICalendar.java
@@ -32,6 +32,8 @@ import ietf.params.xml.ns.icalendar.property.base.text.*;
 import ietf.params.xml.ns.icalendar.property.base.textlist.CategoriesPropType;
 import ietf.params.xml.ns.icalendar.property.base.utcdatetime.CreatedPropType;
 import ietf.params.xml.ns.icalendar.property.base.utcdatetime.DtstampPropType;
+
+import javax.ejb.Schedule;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.*;
@@ -43,10 +45,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javax.ejb.Schedule;
 
 import static ietf.params.xml.ns.icalendar.Constants.ZONE_UTC;
-import static ietf.params.xml.ns.icalendar.EFreqRecurType.*;
 
 /**
  * A utility class to help interact and manipulate ICalendar classes and
@@ -150,7 +150,7 @@ public class ICalendar {
      * original EVENT are added.
      */
     while (pStart.isBefore(eventStart)) {
-      pStart = pStart.minus(duration);
+      pStart = pStart.plus(duration);
     }
     /**
      * Finally: Scan through the period of interest in FREQ increments. For each
